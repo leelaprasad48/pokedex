4fed4749033fe3efb4646d98117640b97599f9a1
leelaprasad48 <leela.prasad@nineleaps.com>
2019-02-05 19:21:59 +0530

    Added badges to pokemon info

--- src/components/PokemonStats.js
+++ src/components/PokemonStats.js
@@ -6,30 +6,55 @@ function PokemonStats(props) {
         <table className="table">
             <tbody>
             <tr>
+                <td>Id</td>
+                <td><span className="badge badge-primary">{res.id}</span></td>
+            </tr>
+            <tr>
                 <td>Experience</td>
-                <td>{res.base_experience}</td>
+                <td><span className="badge badge-warning">{res.base_experience}</span></td>
+            </tr>
+            <tr>
+                <td>Type</td>
+                <td>{res.types.map(typeType => {
+                    return (<span className="badge badge-type">{typeType.type.name}</span>)
+                })}</td>
+            </tr>
+            <tr>
+                <td>Stats</td>
+                <td>
+                    <table className="table table-borderless table-striped">
+                        <tbody>
+                        {res.stats.map(statType => {
+                            return (
+                                <tr>
+                                    <td><span className="badge badge-statType">{statType.stat.name}</span></td>
+                                    <td><span className="badge badge-statValue">{statType.base_stat}</span></td>
+                                </tr>
+                            );
+                        })}
+                        </tbody>
+                    </table>
+                </td>
             </tr>
             <tr>
                 <td>Height</td>
-                <td>{res.height}</td>
+                <td><span className="badge badge-dark">{res.height}</span></td>
             </tr>
             <tr>
-                <td>Id</td>
-                <td>{res.id}</td>
-            </tr>
-            <tr>
-                <td>Order</td>
-                <td>{res.order}</td>
+                <td>Weight</td>
+                <td><span className="badge badge-dark">{res.weight}</span></td>
             </tr>
             <tr>
                 <td>Moves</td>
                 <td>{res.moves.map(moveType => {
-                    return(<span className="badge badge-pill badge-info">{moveType.move.name}</span>)
+                    return (<span className="badge badge-pill badge-info">{moveType.move.name}</span>)
                 })}</td>
             </tr>
             <tr>
-                <td>Moves</td>
-                <td><span className="badge badge-info">   </span></td>
+                <td>Abilities</td>
+                <td>{res.abilities.map(abilityType => {
+                    return (<span className="badge badge-success">{abilityType.ability.name}</span>)
+                })}</td>
             </tr>
             </tbody>
         </table>
